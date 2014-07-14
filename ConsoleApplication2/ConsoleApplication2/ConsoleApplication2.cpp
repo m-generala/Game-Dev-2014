0fadbfbc2ee82750e73acf37fe6366e38830b23e
m-generala <marti_world@abv.bg>
2014-07-15 02:19:25 +0300

    too much for today

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,5 +1,7 @@
+
+
 // ConsoleApplication2.cpp : Defines the entry point for the console application.
-//
+
 
 #include "stdafx.h"
 #include <iostream>
@@ -8,7 +10,7 @@ int genB(int a){
 	int b;
 	do{
 		b=rand()%10;
-	}while(b==a);
+	}while(b==a||b==7);
 		return b;
 }
 int genC(int a,int b){
@@ -29,30 +31,30 @@ int numcheck(int A,int B,int C,int D,int a,int b,int c,int d)
 {
   int bulls=0,cows=0;
   if(A==b||A==c||A==d){
-	  cows=+1;}
+	  cows+=1;}
   else{ if (A==a)
-	  bulls=+1;}
+	  bulls+=1;}
   if(B==a||B==c||B==d){
-	  cows=+1;}
+	  cows+=1;}
   else{ if (B==b)
-	  bulls=+1;}
+	  bulls+=1;}
   if(C==a||C==b||C==d){
-	  cows=+1;}
+	  cows+=1;}
   else{ if (C==c)
-	  bulls=+1;}
+	  bulls+=1;}
   if(D==a||D==b||D==c){
-	  cows=+1;}
+	  cows+=1;}
   else{ if (D==d)
-	  bulls=+1;}
+	  bulls+=1;}
   if(bulls==4)
   {
-     printf("Congrats!");
+     printf("Congrats!\n");
      return 1;
   }
   else{
      printf("Bulls=%d\nCows=%d\nTry Again!!\n",bulls,cows);
   }
-}
+};
 
 int main(int argc, char* argv[])
 {
